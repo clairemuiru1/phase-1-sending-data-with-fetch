0aa1ad69ae2892939e88c3fec7f3894ae157cbcf
clairemuiru1 <muiruclaire@gmail.com>
2023-10-04 21:05:00 +0300

    "Completed assignment"

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Add your code here
+const submitData = (name, email) => {
+const formData = {
+    userName:'felowfell',
+    userEmail:'felowfell@gmial.com'
+}
+const collectData ={
+    method:"POST",
+    headers: {
+        "content-Type":"application/json",
+        Accept:"application/json"
+    },
+    body:JSON.stringify(formData)
+};
+fetch('http://localhost:3000/users', formData)
+.then((response) =>response.json())
+.then(json => {
+    document.body.innerHTML =json["id"]
+})
+.catch(function (error) {
+    alert("Bad things! Claire!");
+    console.log(error.message);
+  });
+}
